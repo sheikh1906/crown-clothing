b0381eeb1fe98c74af18a9e08609f8391b34a7ab
Sheikh <sheikh1906@gmail.com>
2022-11-21 19:51:43 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+    },
+    {
+      id: 4,
+      title: "Women's",
+    },
+    {
+      id: 5,
+      title: "Men's",
+    },
+  ];
+
   return (
     <div className="categories-container">
-      <div className="category-container">
-        {/* <img/> */}
-        <div className="category-body-container">
-          <h2>Hats</h2>
-          <p>Shop Now</p>
+      {categories.map((category) => (
+        <div className="category-container">
+          <div className="backgroun-image"></div>
+          <div className="category-body-container">
+            <h2>{category.title}</h2>
+            <p>Shop Now</p>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
